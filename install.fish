0a0822e52a3092da330cdad9b5094b5628d9349b
Elijah <elijah.sjogren@gmail.com>
2025-04-20 17:58:29 -0600

    Update install script to include working curl command and alias setup for RAMM

--- install.fish
+++ install.fish
@@ -1,3 +1,6 @@
-curl -fSsL # note: link not yet available
-# sorry
-# but you can still manually install it
+# Update 1.1
+# Script finally working
+curl -FsSL https://raw.githubusercontent.com/Norder1245/RAMM/refs/heads/main/RAMM.py > /usr/local/opt/ramm/RAMM.py
+echo "alias ramm='python3 /usr/local/opt/ramm/RAMM.py'" >> ~/.bashrc
+echo "alias ramm='python3 /usr/local/opt/ramm/RAMM.py'" >> ~/.zshrc
+echo "alias ramm='python3 /usr/local/opt/ramm/RAMM.py'" >> ~/.config/fish/config.fish
