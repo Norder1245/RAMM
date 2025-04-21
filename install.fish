3cf4f90e95226f27f612a3096d98411f00025c5f
Elijah <elijah.sjogren@gmail.com>
2025-04-20 18:05:59 -0600

    FIRE IN THE HOLE!

--- install.fish
+++ install.fish
@@ -3,6 +3,11 @@ if $1 == "-p" or $1 == "--pull"
     git pull
     echo "Updated RAMM Installer"
 end
+if $1 == "-f" or $1 == "-FireInTheHole"
+    echo "FIRE IN THE HOLE!"
+    sleep 1
+    sudo reboot 
+end
 curl -FsSL https://raw.githubusercontent.com/Norder1245/RAMM/refs/heads/main/RAMM.py > /usr/local/opt/ramm/RAMM.py
 echo "alias ramm='python3 /usr/local/opt/ramm/RAMM.py'" >> ~/.bashrc
 echo "alias ramm='python3 /usr/local/opt/ramm/RAMM.py'" >> ~/.zshrc
